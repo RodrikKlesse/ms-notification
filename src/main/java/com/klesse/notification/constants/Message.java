26fb01e6fbec054a2c8a06ba83ff68db517e38dd
RodrikKlesse <rodrik_hsk@hotmail.com>
2025-02-14 17:34:53 -0300

    Tweaks to finish the project

--- src/main/java/com/klesse/notification/constants/Message.java
+++ src/main/java/com/klesse/notification/constants/Message.java
@@ -6,4 +6,6 @@ public class Message {
     public static final String COMPLETED_PROPOSAL_ANALYSIS_APPROVED = "Dear %s, your proposal analysis has been completed by our team and it was APPROVED.";
 
     public static final String COMPLETED_PROPOSAL_ANALYSIS_DENIED = "Dear %s, your proposal analysis has been completed by our team and unfortunately it was Denied. NOTE: %s";
+
+    public static final String PROPOSAL_ANALYSIS_DENIED = "Dear %s, after analysis, it was not possible to proceed with the proposal";
 }
--- src/main/java/com/klesse/notification/listener/CompletedProposalListener.java
+++ src/main/java/com/klesse/notification/listener/CompletedProposalListener.java
@@ -7,6 +7,8 @@ import lombok.AllArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
+import java.util.Objects;
+
 @AllArgsConstructor
 @Component
 public class CompletedProposalListener {
@@ -15,12 +17,15 @@ public class CompletedProposalListener {
 
     @RabbitListener(queues = "${rabbitmq.queue.completed.proposal.notification}")
     public void completedProposal(Proposal proposal) {
+        String name = proposal.getUsers().getName();
         String message;
 
         if(proposal.getStatus()) {
              message = String.format(Message.COMPLETED_PROPOSAL_ANALYSIS_APPROVED, proposal.getUsers().getName());
-        } else {
+        } else if (Objects.nonNull(proposal.getDescription())){
             message = String.format(Message.COMPLETED_PROPOSAL_ANALYSIS_DENIED, proposal.getUsers().getName(), proposal.getDescription());
+        } else {
+            message = String.format(Message.PROPOSAL_ANALYSIS_DENIED, name);
         }
 
         notificationSNSService.notifier(proposal.getUsers().getPhoneNumber(), message);
